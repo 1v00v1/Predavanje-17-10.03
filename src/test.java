f9b811c5887d5907ab193a9ad22be42bd98a64e4
Iv00vI <goran.ivo11@gmail.com>
2025-03-10 20:21:11 +0100

    zad

--- src/test.java
+++
@@ -1,9 +0,0 @@
-public class test {
-    public static void main(String[] args) {
-        String s ="C:\\Users\\goran\\Documents\\Java-Algebra\\original.pdf";
-        int p = s.lastIndexOf("\\");
-        String a = s.substring(0,p+1);
-        System.out.println(a);
-
-    }
-}
