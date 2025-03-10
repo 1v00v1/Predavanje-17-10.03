6d2a6d4e7ba52a62da9c8c307fba6944c0bab76e
Iv00vI <goran.ivo11@gmail.com>
2025-03-10 20:05:01 +0100

    zad

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 
 // C:\Users\goran\Documents\Java-Algebra\original.pdf
-// String s = "C:\\Users\\goran\\Documents\\Java-Algebra"
+
 public class Main {
     public static void main(String[] args) throws IOException {
         byte[] podaci = new byte[0];
@@ -14,11 +14,14 @@ public class Main {
         FileOutputStream fos = null;
         String outputString;
         String putanja;
+        String lokacija;
         boolean exit = false;
         Scanner s = new Scanner(System.in);
         do {
             System.out.println("Unesite putanju do originalne datoteke:");
+
             putanja = s.nextLine().replace("\\", "\\\\");
+            lokacija = putanja.substring(0,putanja.lastIndexOf("\\")+1);
 
             try {
                 fis = new FileInputStream(putanja);
@@ -35,33 +38,44 @@ public class Main {
         } while (!exit);
         exit = false;
         do {
-            System.out.println("Unesite naziv kopiranje datoteke:");
+            System.out.println("Unesite naziv kopirane datoteke:");
              outputString = s.nextLine().trim();
-            outputString = outputString.replace("\\", "\\\\");
-            exit = true;
+          //  outputString = outputString.replace("\\", "\\\\");
+            outputString = lokacija + outputString;
+            if (outputString.endsWith(".pdf")){
+                exit = true;
+            }else {
+                outputString +=".pdf";
+                exit = true;
+            }
+
             try {
                 fos = new FileOutputStream(outputString);
                 fos.write(podaci);
                 System.out.println("Datoteka uspješno kopirana.");
                 fos.close();
-            } catch (Exception e) {
+            } catch (FileNotFoundException e) {
                 e.getMessage();
+                exit =false;
 
             }
         } while (!exit);
         System.out.println("Želite li isbrisati kopiju datoteke (D/N)?");
         String temp = s.nextLine().trim().toLowerCase();
+        s.close();
         switch (temp){
             case "d"->{
                 try {
                     Files.deleteIfExists(Path.of(outputString));
-                    System.out.println("Datoteka "+outputString+" izbrisana");
-                }catch (FileNotFoundException e){
+                    System.out.println("Datoteka "
+                            +outputString.substring(outputString.lastIndexOf("\\")+1)
+                            +" izbrisana");
+                }catch (IOException e){
                     e.printStackTrace();
                 }
 
             }
-            case "n" -> System.out.println("Datoteka sačuvana");
+            default -> System.out.println("Datoteka sačuvana");
         }
 
 
